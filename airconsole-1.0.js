f717d29b11615d5b5c00ca6efc6d67e606c017cd
Andrin von Rechenberg <andrinvr@gmail.com>
2015-06-30 17:56:45 +0200

    Updating JSDocs

--- airconsole-1.0.js
+++ airconsole-1.0.js
@@ -57,34 +57,34 @@
 
 /**
  * An object containing information about a device in this session.
- * @typedef {object} DeviceState
- * Fields are:
- * {string} uid - The globally unique ID of the user.
- * {string|undefined} custom - Custom device data that this API can set.
- * {string|undefined} nickname - The nickname of the user.
- * {string|undefined} thumbnail - The url to a 40x40 px thumbnail of the user.
- * {boolean|undefined} slow_connection - If the user has a high server latency.
+ * @typedef {object} AirConsole~DeviceState
+ * @property {string} uid - The globally unique ID of the user.
+ * @property {string|undefined} custom - Custom device data that this API can set.
+ * @property {string|undefined} nickname - The nickname of the user.
+ * @property {string|undefined} thumbnail - The url to a 40x40 px thumbnail of the user.
+ * @property {boolean|undefined} slow_connection - If the user has a high server latency.
+ */
+
+/**
+ * An object containing information about a device in this session.
+ * @typedef {object} AirConsole~Config
+ * @property {string} orientation - AirConsole.ORIENTATION_PORTRAIT or
+ *           AirConsole.ORIENTATION_LANDSCAPE.
+ * @property {boolean|undefined} synchronize_time - If set to true, you can
+ *           call getServerTime() to get the time on the game server.
  */
 
 /**
  * Your gateway object to the N-Dream Game Console.
  * @constructor
- * @property {Array.<DeviceState>} devices - An array of the device data of all
- *                                          devices. The position is equal to
- *                                          the device ID of that device (The
- *                                          first element is the screen).
- *                                          An element can be undefined if the
- *                                          has left.
+ * @property {Array.<AirConsole~DeviceState>} devices - An array of the device
+ *           data of all devices. The position is equal to the device ID of
+ *           that device (The first element is the screen).
+ *           An element can be undefined if the has left.
  * @property {number} server_time_offset - The difference between this devices
- *                                         time and the time on the gameserver.
- *                                         Only correct if the opts param has
- *                                         "synchronize_time" set to true and
- *                                         onReady was called.
- * @param {Object} opts - Constructor config. Possible key,values are:
- *   "orientation": Force the screen to a certain orientation. Possible values:
- *                  "portrait", "landscape"
- *   "synchronize_time": If set to true, you can call getServerTime() to get
- *                       the time on the game server.
+ *           time and the time on the gameserver. Only correct if the opts
+ *           param has "synchronize_time" set to true and onReady was called.
+ * @param {AirConsole~Config} opts - Constructor config.
  */
 function AirConsole(opts) {
   opts = opts || {};
@@ -155,15 +155,27 @@ AirConsole.prototype.onMessage = function(from, data) {};
 
 /**
  * The device ID of the game screen.
- * @type {number}
+ * @constant {number}
  */
 AirConsole.SCREEN = 0;
 
 /**
+ * The portrait orientation.
+ * @constant {string}
+ */
+AirConsole.ORIENTATION_PORTRAIT = "portrait";
+
+/**
+ * The landscape orientation.
+ * @constant {string}
+ */
+AirConsole.ORIENTATION_LANDSCAPE = "landscape";
+
+/**
  * Gets called when a device joins/leaves/updates it's DeviceState.
  * @param {number} device_id - the device ID that changed it's DeviceState.
- * @param user_data {DeviceState} - the data of that device. If undefined, the
- *                                 device has left.
+ * @param user_data {AirConsole~DeviceState} - the data of that device.
+ *        If undefined, the device has left.
  */
 AirConsole.prototype.onDeviceStateChange = function(device_id, device_data) {};
 
