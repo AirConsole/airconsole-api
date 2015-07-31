403a69b48287a52d1490c60c5b1e2579a9748fa2
Andrin von Rechenberg <andrinvr@gmail.com>
2015-07-31 09:53:54 +0200

    Split out not forward compatible tests

--- tests/spec/airconsole-1.0-spec.js
+++ tests/spec/airconsole-1.0-spec.js
@@ -122,24 +122,4 @@ describe("API 1.0", function() {
     expect(air_console.onDeviceStateChange).toHaveBeenCalledWith(from, expected_data);
     expect(air_console.devices[from]).toEqual(expected_data);
   });
-
-  it ("should call onReady when window message event is dispatched with action equal ready", function() {
-    var device_id = 8;
-    air_console = new AirConsole();
-    spyOn(air_console, 'onReady');
-    // Trigger Event
-    var fake_event = new Event('message');
-    fake_event.data = {
-      action: "ready",
-      device_id: device_id,
-      devices: [1, 5, 8],
-      code: 1034,
-
-    };
-    window.dispatchEvent(fake_event);
-    //
-    expect(air_console.onReady).toHaveBeenCalledWith(1034, device_id);
-    expect(air_console.devices).toEqual([1, 5, 8]);
-  });
-
 });
