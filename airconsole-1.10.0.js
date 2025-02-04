a8bd6d7c519c52a4e48140e57f30842e26173363
marc-n-dream <143824611+marc-n-dream@users.noreply.github.com>
2025-02-04 10:43:22 +0100

    d Update copyright year

--- airconsole-1.10.0.js
+++ airconsole-1.10.0.js
@@ -1,6 +1,6 @@
 /**
  * AirConsole.
- * @copyright 2024 by N-Dream AG, Switzerland. All rights reserved.
+ * @copyright 2025 by N-Dream AG, Switzerland. All rights reserved.
  * @version 1.10.0
  *
  * IMPORTANT:
