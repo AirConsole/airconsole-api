f614d4aed98276021d07f76b1975b090875bcf4e
marc-n-dream <marc-n-dream@users.noreply.github.com>
2026-05-29 04:33:44 +0200

    docs: Update getConfiguration documentation.

--- beta/airconsole-1.11.0.js
+++ beta/airconsole-1.11.0.js
@@ -296,19 +296,22 @@ AirConsole.prototype.arePlayersSilenced = function () {
 /**
  * Returns the platform capability configuration delivered in the ready event.
  * Use this to branch on device capabilities instead of platform or partner
- * names.  Only available on the screen; controllers receive undefined.
+ * names.  Only available on the screen; throws on controllers.
  * Can only be called after onReady.
  * @return {Object|undefined} An object with:
  *   supportedVideoFormats {string[]} - e.g. ["vp9","h264","vp8"]
  *   transparentVideoSupported {boolean}
  *   unityVideoSupported {boolean}
  *   graphicsQualityTier {string} - "low", "medium", or "high"
- * Returns undefined on controllers or if the platform did not send it.
+ * Throws on controllers. Returns an empty object on the screen if the platform did
+ * not send a configuration payload.
+ * @throws {string} "Only the AirConsole.SCREEN can call getConfiguration!"
+ *   when called from a controller.
  * @since 1.10.0
  */
 AirConsole.prototype.getConfiguration = function () {
   if (this.device_id === AirConsole.SCREEN) {
-    return this.configuration;
+    return this.configuration || {};
   }
 
   throw "Only the AirConsole.SCREEN can call getConfiguration!";
